bf08277b07839323015244d7954e46e971678367
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-08-04 19:03:06 -0300

    styles setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./styles/global";
 import Router from "./routes/Router";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: "#fff",
+
+  background: "#FAFAFA",
+
+  base_title: "#272221",
+  base_subtitle: "#403937",
+  base_text: "#574f4d",
+  base_label: "#8d8686",
+  base_hover: "#d7d5d5",
+  base_button: "#e6e5e5",
+  base_input: "#ededed",
+  base_card: "#f3f2f2",
+
+  primary_dark: "#c47f17",
+  primary: "#dbac2c",
+  primary_light: "#f1e9c9",
+
+  secondary_dark: "#4b2995",
+  secondary: "#8047f8",
+  secondary_light: "#ebe5f9",
+};
